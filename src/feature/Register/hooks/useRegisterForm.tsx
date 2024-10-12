dc347cf13462a41a425904e5689bfa0806ca7403
Ogata Katsuya <130939004+ogatakatsuya@users.noreply.github.com>
2024-10-13 03:08:02 +0900

    エラーハンドリングを追加

--- src/feature/Register/hooks/useRegisterForm.tsx
+++ src/feature/Register/hooks/useRegisterForm.tsx
@@ -1,8 +1,12 @@
 import { zodResolver } from "@hookform/resolvers/zod";
+import { AxiosError } from "axios";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { registerGroupWithSubId } from "../../../client/services.gen";
-import type { CreateGroupRequest } from "../../../client/types.gen";
+import type {
+	CreateGroupRequest,
+	CreateGroupResponse,
+} from "../../../client/types.gen";
 import {
 	type RegisterFormSchemaType,
 	registerFormSchema,
@@ -30,6 +34,10 @@ const useRegisterForm = (quiz_set_id: string | undefined) => {
 			body: request,
 			path: { quiz_set_sub_id: quiz_set_id },
 		});
+		if (response.code === "ERR_BAD_REQUEST") {
+			alert(response.error.detail);
+		}
+		console.log("response", response);
 		if (response.data?.message) {
 			navigation(`/quiz/${quiz_set_id}`);
 		}
